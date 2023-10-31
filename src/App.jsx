fd02c12ca39347cb983bb25632cebf8c6c68d05e
HilmiAliSalsabila <alihilmi93@gmail.com>
2023-11-01 01:19:46 +0700

    Commit pertama

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-[390px] h-[844px] relative bg-gray-200">
+      <div className="w-[390px] h-[527px] left-0 top-[317px] absolute bg-white rounded-[5px] shadow" />
+      <div className="w-[153px] h-[45px] left-[20px] top-[770px] absolute rounded-[5px] border border-stone-500" />
+      <div className="left-[63px] top-[784px] absolute text-stone-500 text-[15px] font-medium font-['Asap Condensed']">Bayar Nanti</div>
+      <div className="w-[180px] h-[45px] left-[189px] top-[770px] absolute bg-stone-500 rounded-[5px] border" style={{ backgroundColor: '#98694F' }} />
+      <div className="w-[92px] h-[23px] left-[210px] top-[784px] absolute text-white text-[15px] font-medium font-['Asap Condensed'] text-justify ">Bayar Sekarang</div>
+      <div className="w-[390px] h-[65px] left-0 top-0 absolute bg-stone-500 shadow" style={{ backgroundColor: '#98694F' }} />
+      <div className="left-[133px] top-[20px] absolute text-white text-xl font-bold font-['Asap Condensed']">Rincian Pesanan</div>
+      <img
+        className="w-[25px] h-[25px] left-[20px] top-[20px] absolute"
+        src="https://s3-alpha-sig.figma.com/img/7036/830f/874ed763620b896bdc0dcd5d36887044?Expires=1699833600&Signature=qpfoqQ3jbgVD92Lesq69~3Tgm-gBFZ9y2vry8B6yB7yXoETI5AnKHmZJglXrgCJ0E8PrPYHdVyAuGUIgEeIQmdsAnb0rJJrITt1ctsflE3CUf5omQiM1kVInJGh6yyNBTsx~G-u8B4IU7864SdJrhxZwjrFJW3O9kJNXfYrS57kH~BurhsvdT1OqVAP9a1~fMRCnz32ibFxPqfyrkXO-ZU0G49ZMhrlLvjBQomNpDPSrTIHkMkc8ODWAUdYbG3PFuKVtumGXugHdlBgQdlGJzz3hiTb7cHEA7rrMqwtZsj~zNxG4s6c8i4Hb~LzoLSW-LqiYY3IcWkup5JxJ9~FUDQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+      />
+      <div className="w-[273px] h-[27px] left-[20px] top-[96px] absolute text-neutral-700 text-[11px] font-normal font-['Asap Condensed'] text-justify">
+        Jln. A.H Nasution No. 847. Cipadung. Ke.. Cibiru, Kota Bandung , Jawa Barat, Indonesia 40614
+      </div>
+      <div className="left-[20px] top-[79px] absolute text-neutral-700 text-[15px] font-bold font-['Asap Condensed']">Java Caffe</div>
+      <div className="left-[346px] top-[86px] absolute text-neutral-700 text-xl font-bold font-['Asap Condensed']">M8</div>
+      <div className="left-[20px] top-[291px] absolute text-neutral-700 text-[15px] font-bold font-['Asap Condensed']">Rincian Pesanan</div>
+      <div className="w-[390px] h-[149px] left-0 top-[132px] absolute bg-white rounded-[5px] shadow" />
+      <div className="w-[349px] h-[55px] left-[20px] top-[149px] absolute rounded-[5px] border border-neutral-400" />
+      <div className="left-[40px] top-[157px] absolute text-neutral-700 text-xs font-normal font-['Asap Condensed']">Nama Pembeli</div>
+      <div className="left-[20px] top-[234px] absolute text-neutral-700 text-xs font-normal font-['Asap Condensed']">Makan di Tempat</div>
+      <div className="left-[20px] top-[327px] absolute text-neutral-700 text-xs font-normal font-['Asap Condensed']">Cafe Latte</div>
+      <div className="left-[20px] top-[344px] absolute text-stone-500 text-[10px] font-normal font-['Asap Condensed']">Hapus</div>
+      <div className="left-[50px] top-[344px] absolute text-stone-500 text-[10px] font-normal font-['Asap Condensed']">Ubah</div>
+      <div className="left-[20px] top-[327px] absolute text-neutral-700 text-xs font-normal font-['Asap Condensed']">Cafe Latte</div>
+      <div className="left-[50px] top-[344px] absolute text-stone-500 text-[10px] font-normal font-['Asap Condensed']">Ubah</div>
+      <div className="left-[100px] top-[328px] absolute text-stone-500 text-[10px] font-medium font-['Asap Condensed']">1x</div>
+      <div className="left-[324px] top-[327px] absolute text-right text-neutral-700 text-xs font-normal font-['Asap Condensed']">RP. 17.000</div>
+      <div className="left-[20px] top-[392px] absolute text-stone-500 text-[10px] font-normal font-['Asap Condensed']">Hapus</div>
+      <div className="left-[20px] top-[375px] absolute text-neutral-700 text-xs font-normal font-['Asap Condensed']">Americano</div>
+      <div className="left-[50px] top-[392px] absolute text-stone-500 text-[10px] font-normal font-['Asap Condensed']">Ubah</div>
+      <div className="left-[100px] top-[376px] absolute text-stone-500 text-[10px] font-medium font-['Asap Condensed']">2x</div>
+      <div className="left-[324px] top-[375px] absolute text-right text-neutral-700 text-xs font-normal font-['Asap Condensed']">RP. 34.000</div>
+      <div className="left-[20px] top-[440px] absolute text-stone-500 text-[10px] font-normal font-['Asap Condensed']">Hapus</div>
+      <div className="left-[20px] top-[423px] absolute text-neutral-700 text-xs font-normal font-['Asap Condensed']">Red Valvet</div>
+      <div className="left-[20px] top-[471px] absolute text-neutral-700 text-xs font-semibold font-['Asap Condensed']">Total</div>
+      <div className="left-[50px] top-[440px] absolute text-stone-500 text-[10px] font-normal font-['Asap Condensed']">Ubah</div>
+      <div className="left-[100px] top-[424px] absolute text-stone-500 text-[10px] font-medium font-['Asap Condensed']">1x</div>
+      <div className="left-[324px] top-[423px] absolute text-right text-neutral-700 text-xs font-normal font-['Asap Condensed']">RP. 16.000</div>
+      <div className="left-[324px] top-[471px] absolute text-right text-neutral-700 text-xs font-semibold font-['Asap Condensed']">RP. 57.000</div>
+      <div className="left-[20px] top-[251px] absolute text-neutral-700 text-xs font-medium font-['Asap Condensed']">31 Oktober 2023 ( 17:18 )</div>
+      <div className="left-[324px] top-[157px] absolute text-neutral-700 text-xs font-medium font-['Asap Condensed']">Hilmi</div>
+      <div className="left-[283px] top-[180px] absolute text-neutral-700 text-xs font-medium font-['Asap Condensed']">085323666527</div>
+      <div className="left-[40px] top-[180px] absolute text-neutral-700 text-xs font-normal font-['Asap Condensed']">No Telepon</div>
+      <div className="w-[390px] h-[0px] left-0 top-[224px] absolute border border-neutral-400"></div>
+      <div className="w-[390px] h-[0px] left-0 top-[365px] absolute border border-neutral-400"></div>
+      <div className="w-[390px] h-[0px] left-0 top-[413px] absolute border border-neutral-400"></div>
+      <div className="w-[390px] h-[0px] left-0 top-[461px] absolute border border-neutral-400"></div>
+    </div>
+  );
+}
+
+export default App;
